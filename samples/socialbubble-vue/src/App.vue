1412a51ae2238614d41f54b6be08e858014ab3df
Jens Reiner <Jens.Reiner@plan-b-gmbh.com>
2021-09-30 16:28:57 +0200

    Socialbubble Application

---
+++ samples/socialbubble-vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Main from './views/Main.vue';
+</script>
+
+<template>
+  <Main/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 8px;
+}
+</style>
---
+++ samples/socialbubble-vue/src/views/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main-layout">
+    <Post v-for="post of posts" :key="post.id" :post="post" />
+  </div>
+</template>
+
+<script>
+import Post from "../components/Post.vue";
+
+export default {
+  components: {
+    Post,
+  },
+
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  async created() {
+    const response = await fetch("http://localhost:8055/items/posts");
+    const posts = await response.json();
+    this.posts = posts.data;
+  },
+};
+</script>
+
+<style>
+.main-layout {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 48px;
+}
+</style>
