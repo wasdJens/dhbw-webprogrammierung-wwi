47ff3730f1c95c2e4e893252128c968065f4f1c2
Jens Reiner <wasdJens@gmail.com>
2021-10-06 22:30:26 +0200

    Patch express beer api to allow cors

--- Express-Samples/express-beer-api/beer-api.js
+++ Express-Samples/express-beer-api/beer-api.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const port = 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 
 const beers = [];
 
