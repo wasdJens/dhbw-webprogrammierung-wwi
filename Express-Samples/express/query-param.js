6946749aed9457a8446e62a24e0949786c2f8d62
Jens Reiner <Jens.Reiner@plan-b-gmbh.com>
2021-12-13 16:22:38 +0100

    Add query parameter example

---
+++ Express-Samples/express/query-param.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+/**
+ * The following endpoint will return the query parameter value
+ * 
+ * It can be used with the following url
+ * http://localhost:3000/articles?name=Test
+ */
+app.get('/articles', (req, res) => {
+    const name = req.query.name;
+    res.send(name);
+});
+
+app.listen(port, () => {
+    console.log('Running...');
+});
