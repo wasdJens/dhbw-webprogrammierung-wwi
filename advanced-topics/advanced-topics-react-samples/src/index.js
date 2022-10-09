82983dd05020be94c0fac755246ad4b59606ca99
Jens Reiner <Jens.Reiner@plan-b-gmbh.com>
2022-10-09 17:52:57 +0200

    ✨ add react table example

--- advanced-topics/advanced-topics-react-samples/src/index.js
+++ advanced-topics/advanced-topics-react-samples/src/index.js
@@ -7,11 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- advanced-topics/advanced-topics-react-samples/src/views/Table.jsx
+++ advanced-topics/advanced-topics-react-samples/src/views/Table.jsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
+import './Table.css';
+
 export function Table() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    // `useEffect` is a synchronous operations which means using async would result in a seperate function that needs to be called inside the hook.
+    fetch("https://swapi.dev/api/films")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log('👀 Swapi API Response', json);
+        console.table(json.results);
+        setData(json.results);
+      });
+  }, []);
+
   return (
     <>
-      <h1>TODO</h1>
+      <h1>Star Wars Films</h1>
+      {!data ? (
+        <h2>Loading...</h2>
+      ) : (
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Director</th>
+                <th>Release Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((movie) => (
+                <tr key={movie.episode_id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.director}</td>
+                  <td>{movie.release_date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
     </>
-  )
+  );
 }
