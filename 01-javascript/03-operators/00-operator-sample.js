b5aa3deb1d456d6ca03393ba9eb0b1264255cdf6
Jens Reiner <wasdJens@gmail.com>
2024-10-07 16:57:08 +0200

    fix: wrong sample code

--- 01-javascript/03-operators/00-operator-sample.js
+++ 01-javascript/03-operators/00-operator-sample.js
@@ -1,5 +1,8 @@
 const input = '5';
-const worth = 25;
+const multiplier = 5;
 
-const result = input + worth;
-console.log(result);
+const resultPlus = input + multiplier;
+const resultMultiply = input * multiplier;
+
+console.log(resultPlus);
+console.log(resultMultiply);
